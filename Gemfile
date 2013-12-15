2cd6ebd24cb578b07498f6c353de528f1c259a47
David Manuel <dmanuel@pflag.org>
2013-12-15 13:58:58 -0500

    initial setup

--- Gemfile
+++ Gemfile
@@ -1,20 +1,23 @@
 source 'https://rubygems.org'
 ruby '2.0.0'
 gem 'rails', '3.2.13'
-gem 'sqlite3'
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
 end
 gem 'jquery-rails'
+gem 'annotate'
 gem 'bootstrap-sass', '~> 2.3.2.2'
 gem 'cancan'
 gem 'devise'
+gem 'omniauth'
+gem 'omniauth-google-oauth2'
 gem 'figaro'
 gem 'rolify'
 gem 'simple_form'
 group :development do
+  gem 'pg'
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :rbx]
   gem 'quiet_assets'
@@ -25,9 +28,11 @@ group :development, :test do
   gem 'rspec-rails'
 end
 group :production do
+  gem 'pg'
   gem 'thin'
 end
 group :test do
+  gem 'sqlite3'
   gem 'capybara'
   gem 'cucumber-rails', :require=>false
   gem 'database_cleaner', '1.0.1'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,45 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  name                   :string(255)
+#
+
 class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, 
+         :omniauthable, :omniauth_providers => [:google_oauth2]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :provider, :uid, :name
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+        user = User.create(name: data["name"],
+             email: data["email"],
+             password: Devise.friendly_token[0,20]
+            )
+    end
+    user
+  end
 end
