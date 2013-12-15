d5500d6ebfc26b90010d2523010bab6ef0b8dbac
David Manuel <dmanuel@pflag.org>
2013-12-15 13:25:27 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scholarships::Application.config.secret_token = 'f225a72615ce474d378b4691e92f1481e1ad51d96f47f332db7dad8fb7d8aa1e508881669d894e83124baa05fb9f92d30ad4839812f32a63477ad8fdcef9b5a7'
