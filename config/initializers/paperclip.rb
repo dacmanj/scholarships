82be52c4bf62b135a1e7ca36acbf59071faca9ca
David Manuel <dmanuel@pflag.org>
2014-05-27 16:06:01 -0400

    prevent overwrite of transcript (reverse)

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,5 @@
 # config/initializers/paperclip.rb
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = ':class/:attachment/:id/:filename'
+Paperclip::Attachment.default_options[:path] = ':class/:attachment/:filename'
+# next year: Paperclip::Attachment.default_options[:path] = ':class/:attachment/:id/:filename'
 Paperclip::Attachment.default_options[:s3_permissions] = 'public-read'
